c2513737804babbf5fb923d897afdb10cdeb2c0e
iamrss <webmaster@iamroadsmartsolent.org.uk>
2020-12-20 16:33:22 +0000

    content-page uses own thumbnail code

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,9 +12,25 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<!-- thumbnail -->
-	<?php rss2021_post_thumbnail(); ?>
+	<?php
+	if ( has_post_thumbnail() ) {
+		?><div class="post-thumbnail"><?php
+		the_post_thumbnail(
+			'post-thumbnail',
+			array(
+				'alt' => the_title_attribute(
+					array(
+						'echo' => false,
+					)
+				),
+			)
+		);
+		?></div><?php
+	}
+	?>
+	<?php // rss2021_post_thumbnail(); ?>
 
-	<header class="entry-header"> This is the_title:
+	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
@@ -37,26 +53,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'rss2021' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
